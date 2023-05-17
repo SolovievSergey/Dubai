9a47a2143bca6beddf1bd1c4c5f2d9632a92bdaa
SolovievSergey <sergeisolovei72@gmail.com>
2023-05-17 19:30:18 +0300

    home revision

--- js/script.js
+++ js/script.js
@@ -1,20 +1,49 @@
 console.log('Hello world');
 
+let myLifeStyle = 'freelance'
 
+console.log(myLifeStyle)
 
+myLifeStyle = 'Всё очень даже и не плохо'
 
+console.log(myLifeStyle)
 
+let myAge = 36
+let myNewAge = 18
 
+myAge = myNewAge
 
+console.log(myAge)
 
+let userName = 'Вася'
+let user = 'Федя'
 
+user = userName
+console.log(user)
 
+let userAge = 36;
 
+console.log(userAge)
 
+let userAgeInfo = "Возраст";
 
+console.log(userAgeInfo)
 
+userAgeInfo = "Возраст: 36"
 
+console.log(userAgeInfo)
 
+let userHeight = 145/0;
+
+console.log(userHeight)
+
+let userMan;
+
+console.log(typeof userMan)
+
+let userSize = "45" / "8";
+
+console.log(typeof userSize)
 
 
 
